7435769f99c2574f75bbf27e4f5aeecbfee52708
steveshi-dotcom <st3v5.s2i@gmail.com>
2022-05-14 11:49:12 -0400

    Initial Commit

---
+++ MyDailyLog/MyDailyLogApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyDailyLogApp.swift
+//  MyDailyLog
+//
+//  Created by Steve Shi on 5/14/22.
+//
+
+import SwiftUI
+
+@main
+struct MyDailyLogApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
